c947a19a2c3d8e65bd9c8dad44c4694691364918
Jim <jimdedrummer99@gmail.com>
2021-10-11 10:02:57 +0200

    Sonarcloud, added plugins (mutations/jacoco) for code coverage

--- src/main/java/nl/hu/cisq1/lingo/data/GameRepository.java
+++
@@ -1,8 +0,0 @@
-package nl.hu.cisq1.lingo.data;
-
-import nl.hu.cisq1.lingo.trainer.domain.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-
-}
